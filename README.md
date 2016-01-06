d6113f8affe97692e982f3c44d8e1481299320a0
bharadwaj-raju <bharadwaj-raju@github.com>
2016-01-06 22:24:31 +0530

    Bugfixes, styles

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# explain-this-xkcd
+# Explain this xkcd
 Chrome/Chromium extension to add a button to every xkcd page to link to it's explain xkcd.
--- chrome/explain-this.js
+++ chrome/explain-this.js
@@ -1,93 +1,72 @@
  /* Explain this xkcd - a Chrome/Chromium browser extension
   * Apache License v2
   * author: Bharadwaj Raju
-  */ 
- 
- 
+  */
+
+
 // Check page title to make sure it is xkcd
-if (/^xkcd:\W/.test(document.title)) {
+if (/xkcd:\W/.test(document.title)) {
     // Creating elements
     var explainButton = document.createElement("BUTTON");
+
     var buttonText = document.createTextNode("Explain this");
+
     explainButton.appendChild(buttonText); // Set button title
-    // Appending to DOM 
+
+    // Set CSS styles
+    explainButton.style.background-color = "#6E6E6E";
+    explainButton.style.background-color = "#6E7B91";
+    explainButton.style.color = "#FFF";
+    explainButton.style.border = "1.5px solid #333";
+    explainButton.style.font-size = "16px";
+    explainButton.style.font-weight = "600";
+    explainButton.style.padding = "1.5px 12px";
+    explainButton.style.margin = "0 4px";
+    explainButton.style.text-decoration = "none";
+    explainButton.style.border-radius = "3px";
+    explainButton.style.-webkit-border-radius = "3px";
+    explainButton.style.box-shadow = "0 0 5px 0 gray";
+    explainButton.style.-webkit-box-shadow = "0 0 5px 0 gray";
+
+    // Appending to DOM
     document.body.appendChild(explainButton);
 }
 
-explainButton.onclick = redirectPage();
+explainButton.addEventListener("click", redirectPage);
 
-redirectPage() {
-    
+function redirectPage() {
+
     // Redirect to explain xkcd
-    
+
     var comicName = findComicName();
-    
-    var comicNumber = findComicNumber();
-    
-    createURL(comicNumber,comicName);
-    
+
+    var explainURL = createURL(comicName);
+
+    window.location.assign(explainURL)
 }
 
-findComicName() {
-    
+function findComicName() {
+
     // Find the current comic's name from page title
-    
-    var comicName = document.title();
-    comicName = comicName.replace(^(xkcd: )\s+); // Remove leading "xkcd: "
-    
+
+    var comicName = document.title.toString();
+    comicName = comicName.replace(/xkcd: /i, ""); // Remove leading "xkcd: "
+
     return comicName;
-    
 }
 
-findComicNumber() {
-    
-    // Find the current comic's number from page's HTML
-    
-    /* Why don't we just get the comic number from the URL?
-     * 
-     * Because the comic number isn't mentioned in the xkcd homepage URL (hosting the latest comic)
-     */
-    
-    var searchTerm = "Permanent link to this comic: ",
-    queue = [document.body],
-    curr
-    ;
-    while (curr = queue.pop()) {
-        if (!curr.textContent.match(searchTerm)) continue;
-        for (var i = 0; i < curr.childNodes.length; ++i) {
-            switch (curr.childNodes[i].nodeType) {
-                case Node.TEXT_NODE : // 3
-                    if (curr.childNodes[i].textContent.match(searchTerm)) {
-                        var elementWithComicNumber = curr;
-                        // End of search
-                    }
-                    break;
-                case Node.ELEMENT_NODE : // 1
-                    queue.push(curr.childNodes[i]);
-                    break;
-            }
-        }
-    }
-    
-    // Remove everything except numbers
-    // Conveniently gets comic number
-    var comicNumber = elementWithComicNumber.replace(/[^0-9]/g, "");
-    
-    return comicNumber;
-}
+function createURL(comicName) {
 
-createURL(comicNumber,comicName) {
-    
     // Create an appropriate explain xkcd URL
-    
+
     var baseURL = "http://www.explainxkcd.com/wiki/index.php/";
-    
+
     // An example explain xkcd URL: http://www.explainxkcd.com/wiki/index.php/[comicNumber]:_[comic_Name]
-    
+
     var comicNameAsInURL = comicName.replace(/ /g,"_"); // Comic name with underscores
-    
-    var URL = baseURL.concat(comicNumber,":_",comicNameAsInURL);
-    
+
+    var URL = baseURL.concat(comicNameAsInURL);
+
     return URL;
-    
+
 }
